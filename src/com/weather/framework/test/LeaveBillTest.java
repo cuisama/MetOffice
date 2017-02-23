a8dbfab9bca2e5bc777089f9791a2581eba65af6
Cui,Yongxiang <yxcui@isoftstone.com>
2017-02-23 15:09:46 +0800

    axcuian

--- src/com/weather/framework/test/LeaveBillTest.java
+++ src/com/weather/framework/test/LeaveBillTest.java
@@ -1,9 +1,7 @@
 package com.weather.framework.test;
 
-import org.activiti.engine.ProcessEngine;
 import org.junit.Test;
 
-import com.sun.xml.internal.ws.api.pipe.Engine;
 
 public class LeaveBillTest {
 	
